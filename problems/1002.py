21efe8716bd2ff3401539246274ebd7120bf937f
DevRuby <60344644+dev-ruby@users.noreply.github.com>
2023-04-27 15:22:11 +0900

    1002 - 터렛

--- problems/1002.py
+++ problems/1002.py
@@ -1,10 +1,24 @@
 # 1002 - 터렛
 
 import sys
+import math
 
 
 def solve():
     input = sys.stdin.readline
+    n = int(input())
+
+    for _ in range(n):
+        x1, y1, r1, x2, y2, r2 = map(int, input().split())
+        distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+        if distance == 0 and r1 == r2:
+            print(-1)
+        elif abs(r1 - r2) == distance or r1 + r2 == distance:
+            print(1)
+        elif abs(r1 - r2) < distance < (r1 + r2):
+            print(2)
+        else:
+            print(0)
 
 
 if __name__ == "__main__":
