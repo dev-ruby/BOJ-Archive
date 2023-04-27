8a96e3099e6249fcdf1d29d4bf4a5ebaec6dd806
DevRuby <60344644+dev-ruby@users.noreply.github.com>
2023-04-27 15:27:26 +0900

    1036 - 36진수

---
+++ problems/1036.py
@@ -0,0 +1,50 @@
+# 1036 - 36진수
+
+import sys
+
+
+def solve():
+    input = sys.stdin.readline
+    N = int(input())
+
+    count = dict()
+
+    for i in list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+        count[i] = 0
+    n_list = list()
+    for _ in range(N):
+        n_list.append(sys.stdin.readline().strip())
+
+    K = int(input())
+
+    for i in n_list:
+        for j in range(len(i)):
+            count[i[::-1][j]] += 36**j
+
+    for i in count.keys():
+        count[i] = count[i] * (35 - int(i, 36))
+
+    count = sorted(count.items())
+    count.sort(key=lambda x: -x[1])
+
+    for i in count[:K]:
+        for j in range(len(n_list)):
+            n_list[j] = n_list[j].replace(i[0], "Z")
+
+    s = 0
+
+    for i in n_list:
+        s += int(i, 36)
+
+    print(to_36(s))
+
+
+def to_36(N):
+    d = list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    a, b = N // 36, N % 36
+    w = d[b]
+    return to_36(a) + w if a != 0 else w
+
+
+if __name__ == "__main__":
+    solve()
